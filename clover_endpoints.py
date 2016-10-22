a80306b49aa3b3f82347cacc2271d1a61debcb61
Agnieszka Szefer <aszefer@circleup.com>
2016-10-22 16:27:05 -0700

    Creted a dummy order.

--- clover_endpoints.py
+++ clover_endpoints.py
@@ -14,7 +14,6 @@ CUSTOMER_ID = 'W5SG4957ECB7Y'
 CUSTOMER_INFO = {
 	'firstName': 'Agnieszka',
 	'lastName': 'Szefer',
-	# 'emailAddresses': ['agnieszka.m.szefer@gmail.com'],
 	'cards': [
 		{
 		  "last4": '0119',
@@ -27,6 +26,37 @@ CUSTOMER_INFO = {
 	'marketingAllowed': True,
 }
 
+ORDER_INFO = {
+	'payments': [
+		{
+			'amount': 10,
+			"cardTransaction": {
+		        "last4": "0119",
+		        "first6": "541333",
+		        "vaultedCard": {
+		          "last4": "0119",
+		          "cardholderName": "TEST/CARD",
+		          "first6": "541333",
+		          "expirationDate": "12/18",
+		        },
+		        "cardholderName": "TEST/CARD",
+      		},
+      		"device": {
+		        "id": ""
+		    },
+		}
+	],
+	'authorizations': [],
+	# "createdTime": None,
+	 "customers": [
+	 	{
+	 		'id': 'W5SG4957ECB7Y',
+	 	}
+	],
+	"device": {
+    	"id": ""
+  	}
+}
 
 def api_url(resource_path):
     """Generate the URL for an API call"""
@@ -89,11 +119,35 @@ def create_customer_for_merchant(merchant_id, customer_info=None):
 	return json_response
 
 
-def update_customer(merchant_id, customer_id=None):
-	url = api_url('/v3/merchants/' + merchant_id + '/customers/' + CUSTOMER_ID)
+# def get_customer_info(merchant_id, customer_id, order_id=None):
+# 	url = api_url('/v3/merchants/' + merchant_id + '/customers/' + customer_id)
+
+# 	resp = requests.get(url, data=json.dumps(customer_info))
+
+# 	print "Got customer for merchant:"
+# 	json_response = resp.json()
+# 	print json_response
+# 	return json_response
+
+
+def update_customer(merchant_id, customer_id, customer_info):
+	url = api_url('/v3/merchants/' + merchant_id + '/customers/' + customer_id)
 	print "Updating a customer for merchant..."
-	resp = requests.post(url, data=json.dumps(CUSTOMER_INFO))
+	resp = requests.get(url, data=json.dumps(customer_info))
 	print "Updated customer for merchant:"
+	json_response = resp.json()
+	print json_response
+	return json_response
+
+
+# def create_payment(merchant_id, device_id, order_id):
+
+
+def create_order(merchant_id, order_info):
+	url = api_url('/v3/merchants/' + merchant_id + '/orders')
+	print "Creating an order for merchant..."
+	resp = requests.post(url, data=json.dumps(order_info))
+	print "Created an order for merchant:"
 	json_response = resp.json()
 	print json_response
 	return json_response
@@ -109,7 +163,15 @@ if __name__ == '__main__':
     get_all_merchant_customers(MERCHANT_ID)
     print 
     # create_customer_for_merchant(MERCHANT_ID, None)
-    update_customer(MERCHANT_ID, CUSTOMER_ID)
-    
+    update_customer(MERCHANT_ID, CUSTOMER_ID, CUSTOMER_INFO)
+    print
+
+    # get_customer_info(MERCHANT_ID, CUSTOMER_ID)
+    print 
+
+    create_order(MERCHANT_ID, ORDER_INFO)
+    print 
+
+
 
     
